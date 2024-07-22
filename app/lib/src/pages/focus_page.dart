6f1d6b9ef39d271cebe560c60fbfb7c249d432f4
Sahil Yeole <sahilyeole93@gmail.com>
2024-07-22 21:26:20 +0530

    hide app bar (temp)
    Signed-off-by: Sahil Yeole <sahilyeole93@gmail.com>

--- app/lib/src/pages/focus_page.dart
+++ app/lib/src/pages/focus_page.dart
@@ -143,33 +143,33 @@ class _FocusPageState extends State<FocusPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.deepPurple,
-      appBar: AppBar(
-        backgroundColor: Colors.deepPurple,
-        leading: const MenuButton(),
-        title: _focusModeHandler(),
-        actions: [
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.grey,
-              borderRadius: BorderRadius.circular(10),
-            ),
-            padding: const EdgeInsets.all(10),
-            child: const Row(
-              children: [
-                Icon(Icons.sports_score_rounded),
-                SizedBox(width: 5),
-                Text(
-                  '1000',
-                  style: TextStyle(
-                    fontSize: 15,
-                    color: Colors.black,
-                  ),
-                )
-              ],
-            ),
-          ),
-        ],
-      ),
+      // appBar: AppBar(
+      //   backgroundColor: Colors.deepPurple,
+      //   leading: const MenuButton(),
+      //   title: _focusModeHandler(),
+      //   actions: [
+      //     Container(
+      //       decoration: BoxDecoration(
+      //         color: Colors.grey,
+      //         borderRadius: BorderRadius.circular(10),
+      //       ),
+      //       padding: const EdgeInsets.all(10),
+      //       child: const Row(
+      //         children: [
+      //           Icon(Icons.sports_score_rounded),
+      //           SizedBox(width: 5),
+      //           Text(
+      //             '1000',
+      //             style: TextStyle(
+      //               fontSize: 15,
+      //               color: Colors.black,
+      //             ),
+      //           )
+      //         ],
+      //       ),
+      //     ),
+      //   ],
+      // ),
       body: Center(
         child: _isFocusMode
             ? const Text("Timer")
