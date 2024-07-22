9566514d3f082d22b9a5360bdc9ae21958e76a30
Sahil Yeole <sahilyeole93@gmail.com>
2024-07-22 17:50:56 +0530

    fix: ip for server
    Signed-off-by: Sahil Yeole <sahilyeole93@gmail.com>

--- server/src/main.rs
+++ server/src/main.rs
@@ -4,7 +4,7 @@ use kairos::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:3333").expect("Failed to bind random port");
+    let listener = TcpListener::bind("0.0.0.0:3333").expect("Failed to bind random port");
     println!(
         "Listening on port {}",
         listener.local_addr().unwrap().port()
