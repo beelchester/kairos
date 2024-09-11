42a0e2c7f9115109a83c01db96101fa0c92da7ca
Sahil Yeole <sahilyeole93@gmail.com>
2024-09-11 15:04:11 +0530

    fix: build
    Signed-off-by: Sahil Yeole <sahilyeole93@gmail.com>

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:kairos/src/global_states.dart';
 import 'package:kairos/src/pages/focus_page.dart';
 import 'package:provider/provider.dart';
 
 Future main() async {
-  await dotenv.load(fileName: ".env");
   runApp(ChangeNotifierProvider(
       create: (context) => GlobalStates(), child: const MyApp()));
 }
